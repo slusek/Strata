3d02fd317cc8671e38c29925d9d13783f300a85b
Tom Wilberding <tom@wilberding.com>
2015-06-25 12:46:23 -0500

    Pulled curve calibration out of analytics
    Passing in a Strata NodalCurve is a first step at refactoring the curves for Cds to be more consistent with the rest of Strata.
    The calibration should ultimately be in a market data function so we can leverage the existing ParRates and NodalCurve classes
    and remain compatible with the Scenario framework to shock both par rates and calibrated zero/hazard rates

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/credit/IsdaCdsPricer.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/credit/IsdaCdsPricer.java
@@ -1,24 +1,30 @@
 /**
  * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
- *
+ * <p>
  * Please see distribution for license.
  */
 package com.opengamma.strata.pricer.credit;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.List;
-
 import com.google.common.collect.Lists;
+import com.opengamma.analytics.financial.credit.isdastandardmodel.ISDACompliantCreditCurve;
+import com.opengamma.analytics.financial.credit.isdastandardmodel.ISDACompliantYieldCurve;
 import com.opengamma.strata.basics.currency.CurrencyAmount;
 import com.opengamma.strata.basics.date.Tenor;
 import com.opengamma.strata.finance.credit.ExpandedCds;
 import com.opengamma.strata.market.curve.CurveMetadata;
 import com.opengamma.strata.market.curve.IsdaCreditCurveParRates;
 import com.opengamma.strata.market.curve.IsdaYieldCurveParRates;
+import com.opengamma.strata.market.curve.NodalCurve;
 import com.opengamma.strata.market.curve.TenorCurveNodeMetadata;
 import com.opengamma.strata.market.sensitivity.CurveCurrencyParameterSensitivities;
 import com.opengamma.strata.market.sensitivity.CurveCurrencyParameterSensitivity;
+import org.joda.beans.MetaBean;
+import org.joda.beans.Property;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Pricer for for CDS products using the ISDA methodology.
@@ -39,15 +45,16 @@ public class IsdaCdsPricer {
   private static final double ONE_BPS = 0.0001d;
 
   //-------------------------------------------------------------------------
+
   /**
    * Calculates the present value of the expanded CDS product.
    * <p>
    * The present value of the CDS is the present value of all cashflows as of the valuation date.
    *
-   * @param product  expanded CDS product
+   * @param product             expanded CDS product
    * @param yieldCurveParRates  par rate curve points of the ISDA discount curve to use
-   * @param creditCurveParRates  par spread rate curve points of the ISDA spread curve to use
-   * @param valuationDate  date to use when calibrating curves and calculating the result
+   * @param creditCurveParRates par spread rate curve points of the ISDA spread curve to use
+   * @param valuationDate       date to use when calibrating curves and calculating the result
    * @return present value of fee leg and any up front fee
    */
   public CurrencyAmount presentValue(
@@ -57,17 +64,140 @@ public class IsdaCdsPricer {
       LocalDate valuationDate) {
 
     double recoveryRate = creditCurveParRates.getRecoveryRate();
-    return IsdaCdsHelper.price(valuationDate, product, yieldCurveParRates, creditCurveParRates, recoveryRate);
+    double scalingFactor = creditCurveParRates.getScalingFactor();
+    ISDACompliantYieldCurve yieldCurve = IsdaCdsHelper.createIsdaDiscountCurve(valuationDate, yieldCurveParRates);
+    ISDACompliantCreditCurve creditCurve = IsdaCdsHelper.createIsdaCreditCurve(valuationDate, creditCurveParRates, yieldCurve, recoveryRate);
+
+    return IsdaCdsHelper.price(valuationDate, product, convert(yieldCurve), convert(creditCurve), recoveryRate, scalingFactor);
+  }
+
+  private NodalCurve convert(ISDACompliantYieldCurve yieldCurve) {
+    return new NodalCurve() {
+      @Override
+      public double[] getXValues() {
+        return yieldCurve.getT();
+      }
+
+      @Override
+      public double[] getYValues() {
+        return yieldCurve.getRt();
+      }
+
+      @Override
+      public NodalCurve withYValues(double[] values) {
+        return null;
+      }
+
+      @Override
+      public CurveMetadata getMetadata() {
+        return null;
+      }
+
+      @Override
+      public int getParameterCount() {
+        return 0;
+      }
+
+      @Override
+      public double yValue(double x) {
+        return 0;
+      }
+
+      @Override
+      public double[] yValueParameterSensitivity(double x) {
+        return new double[0];
+      }
+
+      @Override
+      public double firstDerivative(double x) {
+        return 0;
+      }
+
+      @Override
+      public MetaBean metaBean() {
+        return null;
+      }
+
+      @Override
+      public <R> Property<R> property(String s) {
+        return null;
+      }
+
+      @Override
+      public Set<String> propertyNames() {
+        return null;
+      }
+    };
+  }
+
+  private NodalCurve convert(ISDACompliantCreditCurve creditCurve) {
+    return new NodalCurve() {
+      @Override
+      public double[] getXValues() {
+        return creditCurve.getT();
+      }
+
+      @Override
+      public double[] getYValues() {
+        return creditCurve.getRt();
+      }
+
+      @Override
+      public NodalCurve withYValues(double[] values) {
+        return null;
+      }
+
+      @Override
+      public CurveMetadata getMetadata() {
+        return null;
+      }
+
+      @Override
+      public int getParameterCount() {
+        return 0;
+      }
+
+      @Override
+      public double yValue(double x) {
+        return 0;
+      }
+
+      @Override
+      public double[] yValueParameterSensitivity(double x) {
+        return new double[0];
+      }
+
+      @Override
+      public double firstDerivative(double x) {
+        return 0;
+      }
+
+      @Override
+      public MetaBean metaBean() {
+        return null;
+      }
+
+      @Override
+      public <R> Property<R> property(String s) {
+        return null;
+      }
+
+      @Override
+      public Set<String> propertyNames() {
+        return null;
+      }
+    };
   }
 
   //-------------------------------------------------------------------------
+
   /**
    * Calculates the scalar PV change to a 1 basis point shift in par interest rates.
    *
-   * @param product  expanded CDS product
+   * @param product             expanded CDS product
    * @param yieldCurveParRates  par rate curve points of the ISDA discount curve to use
-   * @param creditCurveParRates  par spread rate curve points of the ISDA spread curve to use
-   * @param valuationDate  date to use when calibrating curves and calculating the result
+   * @param creditCurveParRates par spread rate curve points of the ISDA spread curve to use
+   * @param valuationDate       date to use when calibrating curves and calculating the result
    * @return present value of fee leg and any up front fee
    */
   public CurrencyAmount ir01ParallelPar(
@@ -85,10 +215,10 @@ public class IsdaCdsPricer {
   /**
    * Calculates the vector PV change to a series of 1 basis point shifts in par interest rates at each curve node.
    *
-   * @param product  expanded CDS product
+   * @param product             expanded CDS product
    * @param yieldCurveParRates  par rate curve points of the ISDA discount curve to use
-   * @param creditCurveParRates  par spread rate curve points of the ISDA spread curve to use
-   * @param valuationDate  date to use when calibrating curves and calculating the result
+   * @param creditCurveParRates par spread rate curve points of the ISDA spread curve to use
+   * @param valuationDate       date to use when calibrating curves and calculating the result
    * @return present value of fee leg and any up front fee
    */
   public CurveCurrencyParameterSensitivities ir01BucketedPar(
@@ -116,13 +246,14 @@ public class IsdaCdsPricer {
   }
 
   //-------------------------------------------------------------------------
+
   /**
    * Calculates the scalar PV change to a 1 basis point shift in par credit spread rates.
    *
-   * @param product  expanded CDS product
+   * @param product             expanded CDS product
    * @param yieldCurveParRates  par rate curve points of the ISDA discount curve to use
-   * @param creditCurveParRates  par spread rate curve points of the ISDA spread curve to use
-   * @param valuationDate  date to use when calibrating curves and calculating the result
+   * @param creditCurveParRates par spread rate curve points of the ISDA spread curve to use
+   * @param valuationDate       date to use when calibrating curves and calculating the result
    * @return present value of fee leg and any up front fee
    */
   public CurrencyAmount cs01ParallelPar(
@@ -140,10 +271,10 @@ public class IsdaCdsPricer {
   /**
    * Calculates the vector PV change to a series of 1 basis point shifts in par credit spread rates at each curve node.
    *
-   * @param product  expanded CDS product
+   * @param product             expanded CDS product
    * @param yieldCurveParRates  par rate curve points of the ISDA discount curve to use
-   * @param creditCurveParRates  par spread rate curve points of the ISDA spread curve to use
-   * @param valuationDate  date to use when calibrating curves and calculating the result
+   * @param creditCurveParRates par spread rate curve points of the ISDA spread curve to use
+   * @param valuationDate       date to use when calibrating curves and calculating the result
    * @return present value of fee leg and any up front fee
    */
   public CurveCurrencyParameterSensitivities cs01BucketedPar(
