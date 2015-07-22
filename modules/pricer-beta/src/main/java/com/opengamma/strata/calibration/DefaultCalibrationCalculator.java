28219494e6bc20db30861a3f9e15bff61380c354
marc-opengamma <marc@opengamma.com>
2015-07-22 11:22:17 +0100

    Calibration: Small increment on the DefaultCalibrationCalculator.

--- modules/pricer-beta/src/main/java/com/opengamma/strata/calibration/DefaultCalibrationCalculator.java
+++ modules/pricer-beta/src/main/java/com/opengamma/strata/calibration/DefaultCalibrationCalculator.java
@@ -5,9 +5,35 @@
  */
 package com.opengamma.strata.calibration;
 
+import java.util.function.BiFunction;
+
+import com.opengamma.strata.finance.rate.deposit.IborFixingDepositTrade;
+import com.opengamma.strata.finance.rate.fra.FraTrade;
+import com.opengamma.strata.finance.rate.swap.SwapTrade;
+import com.opengamma.strata.market.sensitivity.PointSensitivities;
+import com.opengamma.strata.pricer.rate.RatesProvider;
+import com.opengamma.strata.pricer.rate.deposit.DiscountingIborFixingDepositProductPricer;
+import com.opengamma.strata.pricer.rate.fra.DiscountingFraProductPricer;
+
 public class DefaultCalibrationCalculator extends CalibrationCalculator {
   
-  public final DefaultCalibrationCalculator DEFAULT = new CalibrationCalculator(
-      (fixing, p) -> DiscountingIborFutureTradePricer.);
+  private DefaultCalibrationCalculator(
+      BiFunction<IborFixingDepositTrade, RatesProvider, Double> fixingValue, 
+      BiFunction<FraTrade, RatesProvider, Double> fraValue,
+      BiFunction<SwapTrade, RatesProvider, Double> swapValue, 
+      BiFunction<IborFixingDepositTrade, RatesProvider, PointSensitivities> fixingSensitivity,
+      BiFunction<FraTrade, RatesProvider, PointSensitivities> fraSensitivity, 
+      BiFunction<SwapTrade, RatesProvider, PointSensitivities> swapSensitivity) {
+    super(fixingValue, fraValue, swapValue, fixingSensitivity, fraSensitivity, swapSensitivity);
+  }
 
+  public final DefaultCalibrationCalculator DEFAULT = new DefaultCalibrationCalculator(
+      (fixing, p) -> DiscountingIborFixingDepositProductPricer.DEFAULT.parSpread(fixing.getProduct(), p), 
+      (fra, p) -> DiscountingFraProductPricer.DEFAULT.parSpread(fra.getProduct(), p),
+      (swap, p) -> DiscountingSwapProductPricer.DEFAULT.parSpread(swap, p),
+      fixingSensitivity, fraSensitivity, swapSensitivity);
+
+  
+  
+  
 }
