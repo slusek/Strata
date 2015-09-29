6bc511ce5add13d0b67bf42edfc3c089fd2ef32c
Yuki Iwashita <yuki@opengamma.com>
2015-09-29 15:28:29 +0100

    swaption, physical settlement

---
+++ modules/finance/src/main/java/com/opengamma/strata/finance/rate/swaption/SwaptionSettlementMethod.java
@@ -0,0 +1,18 @@
+package com.opengamma.strata.finance.rate.swaption;
+
+
+/**
+ * An interface that can return the settlement type and settlement method of swaptions.
+ */
+public interface SwaptionSettlementMethod {
+
+  /**
+   * Gets the settlement type of swaption. 
+   * <p>
+   * The settlement type is cash settlement or physical settlement, defined in {@link SettlementType}.
+   * 
+   * @return the settlement type
+   */
+  public SettlementType getSettlementType();
+
+}
